c608f91ff1d89955369d45a668addd424ba0dbb8
swapnilblues <swapnilbhaumikcfc@gmail.com>
2020-04-27 20:28:26 -0400

    Add Todo working

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -15,12 +15,26 @@ const actions = {
             .then(res => commit('setTodos', res))
         // const response = await axios.get('http://jsonplaceholder.typicode.com/todos')
         // commit('setTodos', response.data)
+    },
+    async addTodo({commit}, title) {
+        await fetch('http://jsonplaceholder.typicode.com/todos',{
+            method : "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: title,
+                completed: false
+            })
+        }).then(res => res.json())
+            .then(res => commit('newTodo', res))
     }
 
 }
 
 const mutations = {
-    setTodos: (state, todos) => (state.todos = todos)
+    setTodos: (state, todos) => (state.todos = todos),
+    newTodo: (state, todo) => state.todos.unshift(todo)
 }
 
 export default {
